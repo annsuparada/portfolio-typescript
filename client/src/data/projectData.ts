ea4c5e2dfe6d2a1f001cd65bb09d65cd3a2eb294
Ann <ann.suparada@gmail.com>
2023-11-15 15:22:34 -0800

    update project data

--- client/src/data/projectData.ts
+++ client/src/data/projectData.ts
@@ -13,62 +13,50 @@ export const projects = [
   },
   {
     imageUrl:
-      'https://res.cloudinary.com/dejr86qx8/image/upload/v1631284662/d2d_kou7qh.jpg',
-    alt: 'Draft2Digital',
-    projectName: 'Draft2Digital',
+      'https://res.cloudinary.com/dejr86qx8/image/upload/v1700076652/Untitled_design_6_lf6tah.png',
+    alt: 'HiMidLow',
+    projectName: 'HiMidLow',
     projectDescription:
-      "A publishing company provides a simple process to turn authors' documents into professionally-formatted ebooks and release them through all the industry's most powerful sales channels.",
+      'An LA based sonic branding and design agency. Built on the idea that sound should never play second fiddle in the creative process. Focus helping clients  further explore creative ideas.',
     tachStacks:
-      'Develop server-side and client-side features using React, Redux, Python, and Django. Maintain existing features such as document conversion, ebook generation, ebook, and metadata distribution to third parties. Implement new features as the company continues to grow.',
-    projectLink: 'https://www.draft2digital.com/',
+      'Collaborated with a UX designer using Figma to ensure a seamless user experience. Leveraged React and Material UI for front-end development.  Deployed the final product using Netlify for production.',
+    projectLink: 'https://himidlow.com/',
     gitHubLink: '',
   },
   {
     imageUrl:
-      'https://res.cloudinary.com/dejr86qx8/image/upload/v1631284662/d2d_kou7qh.jpg',
-    alt: 'Draft2Digital',
-    projectName: 'Draft2Digital',
+      'https://res.cloudinary.com/dejr86qx8/image/upload/v1597960724/recipeFinder_vrragu.jpg',
+    alt: 'Recipe Finder',
+    projectName: 'Recipe Finder',
     projectDescription:
-      "A publishing company provides a simple process to turn authors' documents into professionally-formatted ebooks and release them through all the industry's most powerful sales channels.",
+      'A recipe search web application that provides directions, ingredients, recipes, and nutritional facts. Users can search by ingredients, nutrition, calories and avoid food allergies.',
     tachStacks:
-      'Develop server-side and client-side features using React, Redux, Python, and Django. Maintain existing features such as document conversion, ebook generation, ebook, and metadata distribution to third parties. Implement new features as the company continues to grow.',
-    projectLink: 'https://www.draft2digital.com/',
+      'Used React/Redux for front end and Ant Design for responsive user interface. Display and layout the information received from a 3rd party APIs. CRUD operations drive the application. Created pages that complete all CRUD operations.',
+    projectLink: 'https://myrecipefinder.netlify.app/',
     gitHubLink: '',
   },
   {
     imageUrl:
-      'https://res.cloudinary.com/dejr86qx8/image/upload/v1631284662/d2d_kou7qh.jpg',
-    alt: 'Draft2Digital',
-    projectName: 'Draft2Digital',
+      'https://res.cloudinary.com/dejr86qx8/image/upload/v1604943238/Untitled_design_4_c8k0p8.jpg',
+    alt: 'Perflo',
+    projectName: 'Perflo',
     projectDescription:
-      "A publishing company provides a simple process to turn authors' documents into professionally-formatted ebooks and release them through all the industry's most powerful sales channels.",
+      'A project team performance analytics tool, helping project leaders increase project success with actionable insights when it matters most. Projects are all about staying on track and preventing potential issues from derailing your project.',
     tachStacks:
-      'Develop server-side and client-side features using React, Redux, Python, and Django. Maintain existing features such as document conversion, ebook generation, ebook, and metadata distribution to third parties. Implement new features as the company continues to grow.',
-    projectLink: 'https://www.draft2digital.com/',
+      'Collaborate with a UX designer. Customized existing HTML, CSS, and Bootstrap. Developed homepage, product page, about page, blog page, and contact page.',
+    projectLink: 'https://www.linkedin.com/company/perflo/',
     gitHubLink: '',
   },
   {
     imageUrl:
-      'https://res.cloudinary.com/dejr86qx8/image/upload/v1631284662/d2d_kou7qh.jpg',
-    alt: 'Draft2Digital',
-    projectName: 'Draft2Digital',
+      'https://res.cloudinary.com/dejr86qx8/image/upload/v1604943757/Untitled_design_3_uy9coh.jpg',
+    alt: 'The Observatory Shop',
+    projectName: 'The Observatory Shop',
     projectDescription:
-      "A publishing company provides a simple process to turn authors' documents into professionally-formatted ebooks and release them through all the industry's most powerful sales channels.",
+      'Established and managed a New York-based e-commerce platform specializing in retailing a diverse range of products, including yarn, clothing, accessories, tools, candles, books, and workshop services.',
     tachStacks:
-      'Develop server-side and client-side features using React, Redux, Python, and Django. Maintain existing features such as document conversion, ebook generation, ebook, and metadata distribution to third parties. Implement new features as the company continues to grow.',
-    projectLink: 'https://www.draft2digital.com/',
-    gitHubLink: '',
-  },
-  {
-    imageUrl:
-      'https://res.cloudinary.com/dejr86qx8/image/upload/v1631284662/d2d_kou7qh.jpg',
-    alt: 'Draft2Digital',
-    projectName: 'Draft2Digital',
-    projectDescription:
-      "A publishing company provides a simple process to turn authors' documents into professionally-formatted ebooks and release them through all the industry's most powerful sales channels.",
-    tachStacks:
-      'Develop server-side and client-side features using React, Redux, Python, and Django. Maintain existing features such as document conversion, ebook generation, ebook, and metadata distribution to third parties. Implement new features as the company continues to grow.',
-    projectLink: 'https://www.draft2digital.com/',
+      'Used Fixma to design the website. Customized existing HTML, CSS, and jQuery code from Shopify. Created variant selector feature on a single product page. Created a customizable template page. collaborated with the marketing team to researched customers and competitors for analysis and imporve user experience.',
+    projectLink: 'https://www.facebook.com/theobservatoryshop/',
     gitHubLink: '',
   },
 ]
